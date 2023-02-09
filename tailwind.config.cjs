9c235baacfdf80604cb4e7c3f4762931bfde5266
AlessioToniolo <atoniolo76@gmail.com>
2023-02-09 16:30:07 +0000

    Remove Inter Font Family Bug

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,6 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      fontFamily: {
-      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-    },
   },
   },
   plugins: [],
