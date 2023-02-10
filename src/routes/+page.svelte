071681f8c1433adfa93140e8b8a629bbdff88699
AlessioToniolo <atoniolo76@gmail.com>
2023-02-09 22:26:20 -0500

    TED Content Description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,32 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<!--
+    Create a background with a gradient in the colors of the 
+    TED conference that covers the entire background of the screen
+    using Tailwind CSS classes. In the middle, place the content
+    of some text and a button that links to an external website. 
+    Surround this content with a white box with rounded corners.
+-->
+
+<div
+    class="bg-gradient-to-r from-red-600 to-stone-900 min-h-screen flex items-center justify-center"
+>
+    <div class="w-1/8" />
+    <div class="bg-white rounded-lg p-8">
+        <h1 class="text-2xl font-bold mb-4">TED</h1>
+        <p class="mb-4">
+            TED is a nonprofit organization devoted to Ideas Worth Spreading. It
+            started out (in 1984) as a conference bringing together people from
+            three worlds: Technology, Entertainment, Design. Since then its
+            scope has become ever broader. Along with two annual conferences —
+            the TED Conference in Long Beach and Palm Springs each spring, and
+            the TEDGlobal conference in Edinburgh UK each summer — TED includes
+            the award-winning TEDTalks video site, the Open Translation Project
+            and TED Conversations, the inspiring TED Fellows and TEDx programs,
+            and the annual TED Prize.
+        </p>
+        <a
+            href="https://www.ted.com/"
+            class="bg-ted-red text-white rounded-lg px-4 py-2">Learn more</a
+        >
+    </div>
+    <div class="w-1/8" />
+</div>
